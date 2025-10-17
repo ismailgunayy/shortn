14f9a3a85721148db70b8bc4869fe0339810dcd8
İsmail Günay <ismailggunay@gmail.com>
2025-10-17 16:56:47 +0300

    build: remove unnecessary adapter

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, type ServerOptions } from "vite";
 
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 	const cwd = process.cwd();
 	const env = { ...loadEnv(mode, cwd, "VITE_") };
 
-	const options = {
+	const options: ServerOptions = {
 		host: true,
 		port: Number(env.VITE_PORT) || 3024,
 		strictPort: true
