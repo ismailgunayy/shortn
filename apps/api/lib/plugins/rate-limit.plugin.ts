e5306daaec78e6fc58b9f785529fd94774094197
İsmail Günay <ismailggunay@gmail.com>
2026-07-02 17:31:49 +0200

    feat: return proper rate-limiter error

--- apps/api/lib/plugins/rate-limit.plugin.ts
+++ apps/api/lib/plugins/rate-limit.plugin.ts
@@ -1,6 +1,13 @@
 import { App } from "~/types/fastify";
+import { ShortnError } from "~/common/error";
 import fastifyPlugin from "fastify-plugin";
 import fastifyRateLimit from "@fastify/rate-limit";
+
+class RateLimitExceeded extends ShortnError {
+	constructor(cause?: unknown) {
+		super("Too many requests", 429, cause);
+	}
+}
 
 export const rateLimit = fastifyPlugin(async (app: App) => {
 	await app.register(fastifyRateLimit, {
@@ -17,22 +24,25 @@ export const rateLimit = fastifyPlugin(async (app: App) => {
 
 			switch (request.session?.user.authenticatedWith) {
 				case "apiKey":
-					return 5000;
-
-				case "accessToken":
 					return 2500;
 
-				default:
+				case "accessToken":
 					return 1000;
+
+				default:
+					return 15;
 			}
 		},
 		timeWindow: 1000 * 60, // 1 minute,
 		keyGenerator: (request) => {
 			if (request.session?.user) {
-				return `user-${request.session?.user.id}`;
+				return `session-${request.session?.id}`;
 			}
 
 			return request.ip;
+		},
+		errorResponseBuilder: () => {
+			return new RateLimitExceeded();
 		}
 	});
 });
